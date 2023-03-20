6d2fafd8af444fbf8d56b8dd5035447c2401cbe7
khg <poohghg@naver.com>
2023-03-20 20:58:34 +0900

    cc

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ko">
+      {/* xptmxm */}
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* 타이틀은 페이지내에서 공통사용하자 */}
