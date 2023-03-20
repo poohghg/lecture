f96019c521a12fe6235de2204927e5e8d57d97ad
khg <poohghg@naver.com>
2023-03-20 20:59:29 +0900

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* 타이틀은 페이지내에서 공통사용하자 */}
-        {/* <title>next.js 시작하기</title> */}
+        <title>next.js 시작하기</title>
         {/* <meta name="description" content=">next.js 시작하기" /> */}
       </Head>
       <body>
